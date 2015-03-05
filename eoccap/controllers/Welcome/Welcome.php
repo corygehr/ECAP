35d3bc197dac2b38bf3693ad65c62c05ee7d8802
Cory Gehr <gehrc621@gmail.com>
2015-03-04 19:39:23 -0500

    Initial Code Commit

---
+++ eoccap/controllers/Welcome/Welcome.php
@@ -0,0 +1,37 @@
+<?php
+	/**
+	 * Welcome.php
+	 * Contains the Class for the Welcome Controller
+	 *
+	 * @author Cory Gehr
+	 */
+	
+namespace EocCap;
+
+class Welcome extends \Thinker\Framework\Controller
+{
+	/**
+	 * defaultSubsection()
+	 * Returns the default subsection for this Controller
+	 *
+	 * @access public
+	 * @static
+	 * @return string Subsection Name
+	 */
+	public static function defaultSubsection()
+	{
+		return 'info';
+	}
+
+	/**
+	 * info()
+	 * Passes data back for the 'info' subsection
+	 *
+	 * @access public
+	 */
+	public function info()
+	{
+		// Nothing...
+	}
+}
+?>
---
+++ eoccap/controllers/Welcome/html/info.tpl
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Welcome/html/info.tpl
+     * Contains the HTML template for the info subsection
+     *
+     * @author Cory Gehr
+     */
+?>
+<h1>Penn State Emergency Operations Center - Parking Capacity Management</h1>
+<p>
+	Welcome!
+</p>
