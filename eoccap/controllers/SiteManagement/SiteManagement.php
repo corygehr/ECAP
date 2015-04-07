7bdbce9732f67cf81992d6ee91b0c60d44ff631b
Cory Gehr <gehrc621@gmail.com>
2015-04-06 20:24:29 -0400

    Add SiteManagement controller

---
+++ eoccap/controllers/SiteManagement/SiteManagement.php
@@ -0,0 +1,37 @@
+<?php
+	/**
+	 * SiteManagement.php
+	 * Contains the Class for the SiteManagement Controller
+	 *
+	 * @author Cory Gehr
+	 */
+
+namespace EocCap;
+
+class SiteManagement extends \Thinker\Framework\Controller
+{
+	/**
+	 * defaultSubsection()
+	 * Returns the default subsection for this Controller
+	 *
+	 * @access public
+	 * @static
+	 * @return string Subsection Name
+	 */
+	public static function defaultSubsection()
+	{
+		return 'manage';
+	}
+
+	/**
+	 * manage()
+	 * Passes data back for the 'manage' subsection
+	 *
+	 * @access public
+	 */
+	public function manage()
+	{
+
+	}
+}
+?>
---
+++ eoccap/controllers/SiteManagement/html/manage.tpl
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * SiteManagement/html/manage.tpl
+     * Contains the HTML template for the manage subsection
+	*
+     *
+     * @author Cory Gehr
+     */
+?>
+<h1>Site Management</h1>
+<p>
+	Site Management Tasks Below...
+</p>
