12d0642df9cae1897a1d596448641439172f90fb
Cory Gehr <gehrc621@gmail.com>
2015-04-06 20:13:41 -0400

    Add LotInfo controller

---
+++ eoccap/controllers/LotInfo/LotInfo.php
@@ -0,0 +1,37 @@
+<?php
+	/**
+	 * LotInfo.php
+	 * Contains the Class for the LotInfo Controller
+	 *
+	 * @author Cory Gehr
+	 */
+
+namespace EocCap;
+
+class LotInfo extends \Thinker\Framework\Controller
+{
+	/**
+	 * defaultSubsection()
+	 * Returns the default subsection for this Controller
+	 *
+	 * @access public
+	 * @static
+	 * @return string Subsection Name
+	 */
+	public static function defaultSubsection()
+	{
+		return 'view';
+	}
+
+	/**
+	 * view()
+	 * Passes data back for the 'view' subsection
+	 *
+	 * @access public
+	 */
+	public function view()
+	{
+
+	}
+}
+?>
---
+++ eoccap/controllers/LotInfo/html/view.tpl
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * LotInfo/html/view.tpl
+     * Contains the HTML template for the view subsection
+     *
+     * @author Cory Gehr
+     */
+?>
+<h1>Lot {} Information</h1>
+<p>
+	Info below...
+</p>
