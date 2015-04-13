fea754b792e804bc8e53225f6a513ab4cd662cc9
Cory Gehr <gehrc621@gmail.com>
2015-04-13 00:26:41 -0400

    Properly show/hide user add options

--- www/html/psueoc/js/SiteManagement/manage.js
+++ www/html/psueoc/js/SiteManagement/manage.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
 	$('#access_type').on('change', function() {
-		if($('#access_type').val() == 'attendant') {
-			$('#lot').prop("disabled", false);
+		if($('#access_type').val() == '2') {
+			$('select#lot').prop("disabled", false);
+			$('p#lot').slideDown();
 		}
 		else {
-			$('#lot').prop("enabled", true);
+			$('select#lot').prop("disabled", true);
+			$('p#lot').slideUp();
 		}
 	});
+
+	$('#user_list').tablesorter();
 });
