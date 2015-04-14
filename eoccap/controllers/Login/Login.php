632b3cc863d559e1c6c7ed0d30c15575c0d67ccd
Cory Gehr <gehrc621@gmail.com>
2015-04-14 16:07:49 -0400

    Finish login handlers

--- eoccap/controllers/Login/Login.php
+++ eoccap/controllers/Login/Login.php
@@ -61,6 +61,33 @@ class Login extends \Thinker\Framework\Controller
 	}
 
 	/**
+	 * createSession()
+	 * Creates the session
+	 *
+	 * @access private
+	 * @param string $user Username
+	 */
+	private function createSession($user)
+	{
+		global $_DB;
+
+		// Create new user object
+		$user = new User($user);
+
+		if($user)
+		{
+			// Store it in the session
+			$_SESSION['USER'] = $user;
+
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/**
 	 * processLogin()
 	 * Handles the login for each user type
 	 *
@@ -69,17 +96,65 @@ class Login extends \Thinker\Framework\Controller
 	 */
 	private function processLogin($userType)
 	{
+		global $_DB;
+
 		switch($userType)
 		{
 			// Leave these here for testing...
 			case 'administrator':
+				// Verify the username and password
+				$un = \Thinker\Http\Request::post('username');
+				$hash = User::hashPassword(\Thinker\Http\Request::post('password'));
+
+				// Authenticate
+				$query = "SELECT COUNT(1)
+						  FROM users u 
+						  JOIN user_passwords up ON up.username = u.username 
+						  WHERE u.username = ? 
+						  AND up.hash = ? 
+						  AND u.user_type = 1 
+						  AND u.delete_time IS NULL 
+						  LIMIT 1";
+
+				if(!$_DB['eoc_cap_mgmt']->doQueryAns($query, array($un, $hash)))
+				{
+					// Invalid username or password
+					\Thinker\Framework\Notification::push("Invalid username or password, please try again.", "warning");
+					return false;
+				}
+
+				// Add user object to session
+				$_SESSION['USER'] = new User($un);
+
+				// Redirect
 				\Thinker\Http\Redirect::go('LotManagement', 'manage');
 			break;
 
 			case 'attendant':
+				// Verify user exists
+				$un = \Thinker\Http\Request::post('username');
+
+				// Authenticate
+				$query = "SELECT COUNT(1)
+						  FROM users 
+						  WHERE username = ? 
+						  AND user_type = 2 
+						  AND delete_time IS NULL 
+						  LIMIT 1";
+
+				if(!$_DB['eoc_cap_mgmt']->doQueryAns($query, array($un)))
+				{
+					// Invalid username
+					\Thinker\Framework\Notification::push("User token is inactive or does not exist, please try again.", "warning");
+				}
+
+				$_SESSION['USER'] = new User($un);
+
 				\Thinker\Http\Redirect::go('LotConsole', 'manage', array('id' => 1));
 			break;
 		}
+
+		// We've made it this far, so create the session with the user
 	}
 }
 ?>
