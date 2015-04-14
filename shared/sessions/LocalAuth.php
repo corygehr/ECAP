4754e366d34212bacb1a90a48b9187a45149821f
Cory Gehr <gehrc621@gmail.com>
2015-04-14 17:53:59 -0400

    Add Access Control

--- shared/sessions/LocalAuth.php
+++ shared/sessions/LocalAuth.php
@@ -18,8 +18,6 @@ class LocalAuth extends Thinker\Framework\Session
 	{
 		// Call parent constructor
 		parent::__construct();
-
-		// Create user object
 	}
 
 	/**
@@ -33,21 +31,54 @@ class LocalAuth extends Thinker\Framework\Session
 	 */
 	public function auth($objType = 'section', $params = array())
 	{
+		global $_DB;
+
 		switch($objType)
 		{
-			case 'pageSection':
-				return false;
-			break;
-
 			case 'section':
-				return true;
+				// Expecting [0] => SECTION, [1] => SUBSECTION, [2] => array([URL Param Name] => value)
+				if(count($params) >= 2)
+				{
+					$data = array(
+						':section' => $params['section'],
+						':subsection' => $params['subsection'],
+						':username' => $_SESSION['USER']->username
+						);
+
+					$limiters = '';
+
+					// Add additional URL params if exists
+					if(isset($params[3]))
+					{
+						foreach($params[3] as $id => $val)
+						{
+							$limiters .= " AND ((uri.identifier_name = :$id AND (uri.identifier_value = :$val OR uri.identifier_value = '*')))";
+							$data[":$id"] = $id;
+							$data[":$val"] = $val;
+						}
+					}
+
+					// Query
+					$query = "SELECT COUNT(1)
+							  FROM user_rights ur 
+							  LEFT JOIN user_rights_identifiers uri ON uri.right_id = ur.id 
+							  WHERE ur.username = :username 
+							  AND ((ur.s = :section OR ur.s = '*') AND (ur.ss = :subsection OR ur.ss = '*'))
+							  $limiters 
+							  LIMIT 1";
+
+					return $_DB['eoc_cap_mgmt']->doQueryAns($query, $data);
+				}
+				else
+				{
+					throw new Exception("Invalid number of parameters specified for auth().");
+					return false;
+				}
 			break;
 
 			default:
 				return false;
 			break;
 		}
-
-		return true;
 	}
 }
